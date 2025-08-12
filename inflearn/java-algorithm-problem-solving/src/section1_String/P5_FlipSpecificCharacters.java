5c8bc207a5386c1320526bb203fb76d4e83ed4c4
jk <drakedk@naver.com>
2025-08-13 00:02:12 +0900

    [인프런] 문제 5. 특정 문자 뒤집기
    영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고, 특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성하세요.

---
+++ inflearn/java-algorithm-problem-solving/src/section1_String/P5_FlipSpecificCharacters.java
@@ -0,0 +1,38 @@
+package src.section1_String;
+
+import java.util.Scanner;
+
+public class P5_FlipSpecificCharacters {
+    /*특정문자뒤집기*/
+    // 선생님 답
+    public static String solution(String str){
+        String answer="";
+        // 문자배열 만들어주는 함수
+        char[] s = str.toCharArray();
+        int lt = 0, rt = s.length-1;
+        while(lt<rt){
+            if(!Character.isAlphabetic(s[lt])){
+                lt++;
+            } else if(!Character.isAlphabetic(s[rt])){
+                rt--;
+            } else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        // 문자배열을 문자로 변경
+        answer = String.valueOf(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+
+    }
+}
