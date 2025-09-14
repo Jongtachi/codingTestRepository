76eb84058d67da8339cacfdedd7629c97f922e28
jk <drakedk@naver.com>
2025-09-14 21:15:49 +0900

    [인프런] 문제 9. 숫자만 추출
     선생님 방법 추가

--- inflearn/java-algorithm-problem-solving/src/section1_String/P9_ExtractOnlyNumbers.java
+++ inflearn/java-algorithm-problem-solving/src/section1_String/P9_ExtractOnlyNumbers.java
@@ -11,7 +11,14 @@ public class P9_ExtractOnlyNumbers {
         // replace에는 정규식 사용 X
         int number = Integer.valueOf(str.replaceAll("[^0-9]", "")); // 대문자 A-Z가 아닌 것들은 ""로 변환
         answer = String.valueOf(number);
+
+        /*선생님 방법
+        for(char x : str.toCharArray()){
+            if(Character.isDigit(x)) number +=x;
+        }*/
+
         return answer;
+
     }
 
     public static void main(String[] args) {
