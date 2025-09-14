1b33f2db5f64d0b9547bb79a8ae94331d3bb91e8
jk <drakedk@naver.com>
2025-09-14 21:07:30 +0900

    [인프런] 문제 8. 유효한 팰린드롬
     앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팰린드롬이라고 합니다.
    
     문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES", 아니면 “NO"를 출력하는 프로그램을 작성하세요.
    
     단 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않습니다.
    
     알파벳 이외의 문자들의 무시합니다.

---
+++ inflearn/java-algorithm-problem-solving/src/section1_String/P8_ValidPalindrome.java
@@ -0,0 +1,33 @@
+package src.section1_String;
+
+import java.util.Scanner;
+
+
+public class P8_ValidPalindrome {
+    /*팰린드롬: 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열*/
+    public static String solution(String str){
+        String answer = "NO";
+
+        // replaceAll에는 정규식을 사용할 수 있다.
+        // replace에는 정규식 사용 X
+        str = str.toUpperCase().replaceAll("[^A-Z]", ""); // 대문자 A-Z가 아닌 것들은 ""로 변환
+        String tmp = new StringBuilder(str).reverse().toString();
+        if(str.equals(tmp)) answer="YES";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        /*
+         * nextLine
+         * 한 줄 전체를 읽어서 문자열로 반환합니다.
+         * 개행(엔터)을 만나면 그 전까지 입력한 전체 문자열을 가져옵니다.
+         * 즉, "문장 단위"로 입력을 받음.
+         * */
+        String str = sc.nextLine();
+        System.out.println(solution(str));
+    }
+}
