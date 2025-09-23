6d5ce18152757a257a83163661d7f0ca6ab23ee9
jk <drakedk@naver.com>
2025-09-23 22:26:56 +0900

    [인프런] 문제 11. 문자열 압축
    알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는
    
    문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시오.
    
    단 반복횟수가 1인 경우 생략합니다.

---
+++ inflearn/java-algorithm-problem-solving/src/section1_String/P11_StringCompression.java
@@ -0,0 +1,67 @@
+package src.section1_String;
+
+import java.util.Scanner;
+
+
+public class P11_StringCompression {
+    /*11. 문자열 압축*/
+//    public static String solution(String str){
+//        String answer = "";
+//        char[] s = str.toCharArray();
+//        int num = 1;
+//        for(int i=0; i < s.length ;i++){
+//            //System.out.print(s[i]);
+//            if(i==s.length-1) {
+//                if (s[i] != s[i - 1]) {
+//                    answer += s[i];
+//                } else {
+//                    answer += s[i];
+//                    answer += String.valueOf(num);
+//                }
+//            } else {
+//                if (s[i] != s[i + 1]) {
+//                    answer += s[i];
+//                    if (num != 1) {
+//                        answer += String.valueOf(num);
+//                        num = 1;
+//                    }
+//                } else {
+//                    num++;
+//                }
+//            }
+//        }
+//        return answer;
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//        System.out.print(solution(str.toUpperCase()));
+//
+//    }
+    // 강사님 답안
+    public static String solution(String str){
+        String answer = "";
+        str = str+" ";  // 빈문자열 더해줌
+        int cnt=1;
+        for (int i=0; i<str.length()-1;i++){
+            if (str.charAt(i)== str.charAt(i++)) cnt++;
+            else {
+                answer+=str.charAt(i);
+                if(cnt>1) answer+=String.valueOf(cnt);
+                cnt=1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.print(solution(str.toUpperCase()));
+
+    }
+}
